2cacbc5f1c2ad7fcef763814aff78a9452e115e0
René Klatt <rene.klatt@spryker.com>
2025-11-11 17:30:09 +0100

    FRW-11017 As a project engineer i want to access facades clients and other services directly through the di container (#2)
    FRW-11017 As a Project Engineer I want to access Facades, Clients, and other services directly through the DI container

---
+++ src/Spryker/Glue/EventDispatcher/Plugin/Console/EventDispatcherApplicationPlugin.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+declare(strict_types = 1);
+
+namespace Spryker\Glue\EventDispatcher\Plugin\Console;
+
+use Spryker\Service\Container\ContainerInterface;
+use Spryker\Shared\ApplicationExtension\Dependency\Plugin\ApplicationPluginInterface;
+use Spryker\Yves\Kernel\AbstractPlugin;
+
+/**
+ * @method \Spryker\Glue\EventDispatcher\EventDispatcherFactory getFactory()
+ */
+class EventDispatcherApplicationPlugin extends AbstractPlugin implements ApplicationPluginInterface
+{
+    /**
+     * {@inheritDoc}
+     * - Adds an EventDispatcher service to the container.
+     *
+     * @api
+     */
+    public function provide(ContainerInterface $container): ContainerInterface
+    {
+        $container->set('event_dispatcher', function (ContainerInterface $container) {
+            return $this->getFactory()->createEventDispatcher();
+        });
+
+        return $container;
+    }
+}
---
+++ src/Spryker/Zed/EventDispatcher/Communication/Plugin/Console/EventDispatcherApplicationPlugin.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+declare(strict_types = 1);
+
+namespace Spryker\Zed\EventDispatcher\Communication\Plugin\Console;
+
+use Spryker\Service\Container\ContainerInterface;
+use Spryker\Shared\ApplicationExtension\Dependency\Plugin\ApplicationPluginInterface;
+use Spryker\Zed\Kernel\Communication\AbstractPlugin;
+
+/**
+ * @method \Spryker\Zed\EventDispatcher\Communication\EventDispatcherCommunicationFactory getFactory()
+ * @method \Spryker\Zed\EventDispatcher\EventDispatcherConfig getConfig()
+ */
+class EventDispatcherApplicationPlugin extends AbstractPlugin implements ApplicationPluginInterface
+{
+    /**
+     * {@inheritDoc}
+     * - Adds an EventDispatcher service to the container.
+     *
+     * @api
+     */
+    public function provide(ContainerInterface $container): ContainerInterface
+    {
+        $container->set('event_dispatcher', function (ContainerInterface $container) {
+            return $this->getFactory()->createEventDispatcher();
+        });
+
+        return $container;
+    }
+}
